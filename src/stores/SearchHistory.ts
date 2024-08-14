61b8ee5d9d3c87954e32e9b3e3a59e0c28f0ebe5
Alfred Lau <alfred@sulphur.com.sg>
2024-08-15 03:14:02 +0800

    :sparkles: added zustand search history store

---
+++ src/stores/SearchHistory.ts
@@ -0,0 +1,58 @@
+import { addNameToCity } from '@/components/SearchBar';
+import type {
+	SearchHistory,
+	SearchHistoryAction,
+	SearchHistoryState,
+} from '@/types/Search';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useSearchHistoryStore = create<
+	SearchHistoryState & SearchHistoryAction
+>()(
+	persist(
+		(set, get) => ({
+			searchHistory: [],
+			add: (city) =>
+				set(() => ({
+					searchHistory: [
+						{
+							fullName: city.fullName,
+							searchedOn: Date.now(),
+							city,
+						},
+						...get().searchHistory,
+					],
+				})),
+			push: (newHistory) =>
+				set(() => {
+					console.log(get().searchHistory, newHistory);
+					return {
+						searchHistory: [
+							{ ...newHistory, searchedOn: Date.now() },
+							...get().searchHistory.filter(
+								(history) => history.searchedOn !== newHistory.searchedOn,
+							),
+						],
+					};
+				}),
+		}),
+		{
+			name: 'search-history',
+			skipHydration: true,
+			version: 1,
+			migrate(persistedState: any, version) {
+				if (version === 0) {
+					persistedState.searchHistory = persistedState.searchHistory.map(
+						(history: SearchHistory) => ({
+							...history,
+							fullName: addNameToCity(history.city),
+						}),
+					);
+
+					return persistedState;
+				}
+			},
+		},
+	),
+);
