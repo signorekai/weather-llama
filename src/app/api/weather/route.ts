acaeb455280583fbdaedee8368e12921c3383bf8
Alfred Lau <alfred@sulphur.com.sg>
2024-08-15 18:56:55 +0800

    :sparkles: added /api/weather api route

---
+++ src/app/api/weather/route.ts
@@ -0,0 +1,36 @@
+import { type NextRequest } from 'next/server';
+import OpenWeatherMap from 'openweathermap-ts';
+import type { City, CityWithName } from '@/types/City';
+
+export async function GET(request: NextRequest) {
+	const limit = 10;
+	const key = process.env.OPEN_WEATHER_KEY || '';
+
+	const openWeather = new OpenWeatherMap({
+		apiKey: key,
+		units: 'metric',
+	});
+
+	const { searchParams } = request.nextUrl;
+	const lat = searchParams.get('lat');
+	const lng = searchParams.get('lng');
+
+	if (!lat || !lng) {
+		return Response.json(
+			{ error: 'Need latitude and longitude' },
+			{ status: 500 },
+		);
+	} else {
+		console.log(lat, lng);
+		try {
+			const data = await openWeather.getCurrentWeatherByGeoCoordinates(
+				parseFloat(lat),
+				parseFloat(lng),
+			);
+			return Response.json({ data });
+		} catch (error) {
+			console.log(27, error);
+			return Response.error();
+		}
+	}
+}
