60e149d66c94964a5cdd7dfac761873d9ffac5c3
Alfred Lau <alfred@sulphur.com.sg>
2024-08-16 00:58:58 +0800

    :bug: export all units as static as we might want to display certain units regardless of global state

--- src/hooks/units.ts
+++ src/hooks/units.ts
@@ -1,23 +1,23 @@
 import type { UnitType, UnitsInUse } from '@/types/Units';
 import { useAppStore } from '@/stores/App';
 
+export const presetUnits: { [m in UnitsInUse]: { [k in UnitType]: string } } = {
+	speed: {
+		metric: 'm/s',
+		imperial: 'mi/h',
+	},
+	visibility: {
+		metric: 'm',
+		imperial: 'mi',
+	},
+	temperature: {
+		metric: '°C',
+		imperial: '°F',
+	},
+};
+
 export default function useUnits() {
 	const [units] = useAppStore((state) => [state.units]);
-
-	const presetUnits: { [m in UnitsInUse]: { [k in UnitType]: string } } = {
-		speed: {
-			metric: 'm/s',
-			imperial: 'mi/h',
-		},
-		visibility: {
-			metric: 'm',
-			imperial: 'm',
-		},
-		temperature: {
-			metric: '°C',
-			imperial: '°F',
-		},
-	};
 
 	const toReturn: { [m in UnitsInUse]: string } = {
 		speed: presetUnits.speed[units],
