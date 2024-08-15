98e74d9bff7504306a66a5ee0ae56755f278a6da
Alfred Lau <alfred@sulphur.com.sg>
2024-08-15 22:51:16 +0800

    :lipstick: move fetching to the page itself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,117 @@
-import Image from 'next/image';
+'use client';
+import {
+	CurrentResponse,
+	ThreeHourResponse,
+} from 'openweathermap-ts/dist/types';
+import { DateTime } from 'luxon';
+import { useState, useCallback, useEffect } from 'react';
+
 import WeatherToday from '@/components/WeatherToday';
+import { useAppStore } from '@/stores/App';
+import { Forecast, SortedForecast } from '@/types/Forecast';
+import { Card } from '@/components/Card';
+import WeatherForecast from '@/components/WeatherForecast';
 
 export default function Home() {
+	const [currentCity] = useAppStore((state) => [state.currentCity]);
+
+	const [currentWeather, setCurrentWeather] = useState<CurrentResponse | null>(
+		null,
+	);
+
+	const [forecastWeather, setForecastWeather] = useState<SortedForecast | null>(
+		null,
+	);
+
+	const fileForecasts = useCallback(
+		({ data, timezone }: { data: ThreeHourResponse; timezone: number }) => {
+			const stringTimeZone = `UTC${
+				timezone > 0
+					? `+${timezone / 3600}`
+					: timezone < 0
+					? `${timezone / 3600}`
+					: ''
+			}`;
+
+			const today = DateTime.now().setZone(stringTimeZone);
+			today.endOf('day');
+			const days = [
+				today,
+				...[1, 2, 3, 4, 5].map((v) => today.plus({ days: v })),
+			];
+
+			const filedForecasts: { [T: string]: Forecast[] } = {};
+
+			data.list.forEach((forecast) => {
+				const forecastDate = DateTime.fromSeconds(forecast.dt).setZone(
+					stringTimeZone,
+				);
+				days.every((day) => {
+					const isSameDay = forecastDate.hasSame(day, 'day');
+					if (isSameDay) {
+						const date = day.toISODate();
+						if (date) {
+							if (filedForecasts.hasOwnProperty(date) === false) {
+								filedForecasts[date] = [];
+							}
+							filedForecasts[date].push({
+								lt: forecastDate.toLocaleString(DateTime.TIME_24_SIMPLE),
+								localDate: forecastDate.toLocaleString(
+									DateTime.DATE_MED_WITH_WEEKDAY,
+								),
+								...forecast,
+							});
+							return false;
+						} else {
+							return true;
+						}
+					}
+
+					return true;
+				});
+			});
+
+			setForecastWeather(filedForecasts);
+		},
+		[],
+	);
+
+	useEffect(() => {
+		if (currentCity) {
+			const fetchData = async () => {
+				const currentRes = await fetch(
+					`/api/weather/current?lat=${currentCity?.lat}&lng=${currentCity?.lon}`,
+				);
+				const current: { data: CurrentResponse } = await currentRes.json();
+				setCurrentWeather(current.data);
+
+				const forecastRes = await fetch(
+					`/api/weather/forecast?lat=${currentCity?.lat}&lng=${currentCity?.lon}`,
+				);
+				const forecast: { data: ThreeHourResponse } = await forecastRes.json();
+				fileForecasts({ data: forecast.data, timezone: current.data.timezone });
+			};
+			fetchData();
+		}
+	}, [currentCity, fileForecasts]);
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8">
-			<WeatherToday />
+		<main className="flex flex-1 flex-col items-center justify-between p-4 md:p-8">
+			<WeatherToday
+				currentWeather={currentWeather}
+				forecastWeather={forecastWeather}
+			/>
+			{forecastWeather &&
+				Object.keys(forecastWeather)
+					.slice(1)
+					.filter((k) => forecastWeather[k].length > 0)
+					.map((k) => (
+						<WeatherForecast
+							className="mt-4"
+							key={k}
+							forecastWeather={forecastWeather[k]}
+						/>
+					))}
 		</main>
 	);
 }
--- src/components/WeatherToday.tsx
+++ src/components/WeatherToday.tsx
@@ -1,151 +1,206 @@
 'use client';
 import type { CurrentResponse } from 'openweathermap-ts/dist/types';
-import { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { DateTime } from 'luxon';
 import Image from 'next/image';
 
-import { Card } from '@/components/Card';
+import { Card, List, Row } from '@/components/Card';
 import { useAppStore } from '@/stores/App';
 import Temperature from './Temperature';
 import useUnits from '@/hooks/units';
+import { Forecast } from '@/types/Forecast';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export default function WeatherToday() {
+export default function WeatherToday({
+	forecastWeather,
+	currentWeather,
+}: Readonly<{
+	forecastWeather: {
+		[T: string]: Forecast[];
+	} | null;
+	currentWeather: CurrentResponse | null;
+}>) {
 	const [units, currentCity] = useAppStore((state) => [
 		state.units,
 		state.currentCity,
 	]);
 
 	const presetUnits = useUnits();
-
-	const [currentWeather, setCurrentWeather] = useState<CurrentResponse | null>(
-		null,
-	);
-
-	useEffect(() => {
-		if (currentCity) {
-			const fetchData = async () => {
-				const res = await fetch(
-					`/api/weather/current?lat=${currentCity?.lat}&lng=${currentCity?.lon}`,
-				);
-				const { data }: { data: CurrentResponse } = await res.json();
-				setCurrentWeather(data);
-				console.log(27, data);
-			};
-			fetchData();
-		}
-	}, [currentCity]);
-
 	const dt = DateTime.now();
 
 	return (
-		<div className="max-w-screen-md w-full">
-			<Card className="px-5 py-4">
-				<h6 className="mb-0">
-					{currentWeather?.weather ? (
-						<>
-							{dt
-								.setZone(
-									`UTC${currentWeather.timezone > 0 ? '+' : '-'}${
-										currentWeather.timezone / 3600
-									}`,
-								)
-								.toLocaleString(DateTime.DATETIME_FULL)}
-						</>
-					) : (
-						<>{dt.setLocale('en-SG').toLocaleString(DateTime.DATETIME_FULL)}</>
-					)}
-				</h6>
-				<h3 className="inline-block min-w-52">
-					{currentCity?.fullName || <Skeleton />}
-				</h3>
-				<div className="flex flex-row items-center gap-x-3">
-					{currentWeather?.weather[0] ? (
-						<>
-							<div className="w-[100px] h-[100px]">
-								<Image
-									unoptimized
-									width={100}
-									height={100}
-									alt={`Current weather: ${currentWeather?.weather[0].description}`}
-									src={`https://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}@2x.png`}
-								/>
-							</div>
-							<div>
-								<h2 className="">
-									<Temperature units={units} value={currentWeather.main.temp} />
-								</h2>
-								<h5>
-									<span className="inline-block first-letter:uppercase">
-										{currentWeather?.weather[0].description}.
-									</span>{' '}
+		<AnimatePresence mode="wait">
+			{currentWeather && (
+				<motion.div
+					initial={{ opacity: 0, translateX: 40 }}
+					animate={{ opacity: 1, translateX: 0 }}
+					exit={{ opacity: 0, translateX: -40 }}
+					className="max-w-screen-sm w-full"
+					key={currentCity?.fullName}>
+					<Card className="px-5 pt-4 pb-1">
+						<h6 className="mb-0">
+							{currentWeather?.weather ? (
+								<>
+									{dt
+										.setZone(
+											`UTC${
+												currentWeather.timezone > 0
+													? `+${currentWeather.timezone / 3600}`
+													: currentWeather.timezone < 0
+													? `${currentWeather.timezone / 3600}`
+													: ''
+											}`,
+										)
+										.toLocaleString(DateTime.DATETIME_FULL)}
+								</>
+							) : (
+								<>
+									{dt.setLocale('en-SG').toLocaleString(DateTime.DATETIME_FULL)}
+								</>
+							)}
+						</h6>
+						<h3 className="inline-block min-w-52">
+							{currentCity?.fullName || <Skeleton />}
+						</h3>
+						<div className="flex flex-row items-center gap-x-3">
+							{currentWeather?.weather[0] ? (
+								<>
+									<div className="w-[100px] h-[100px]">
+										<Image
+											unoptimized
+											width={100}
+											height={100}
+											alt={`Current weather: ${currentWeather?.weather[0].description}`}
+											src={`https://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}@2x.png`}
+										/>
+									</div>
+									<div>
+										<h2 className="">
+											<Temperature
+												units={units}
+												value={currentWeather.main.temp}
+											/>
+										</h2>
+										<h5>
+											<span className="inline-block first-letter:uppercase">
+												{currentWeather?.weather[0].description}.
+											</span>{' '}
+											<>
+												<span>Feels like </span>
+												<Temperature
+													units={units}
+													value={currentWeather.main.feels_like}
+												/>
+											</>
+										</h5>
+									</div>
+								</>
+							) : (
+								<Skeleton containerClassName="flex-1" height={80}></Skeleton>
+							)}
+						</div>
+						<div className="flex flex-row gap-x-2 md:gap-x-3 text-center pt-1">
+							<div className="flex-1">
+								{currentWeather?.weather[0] ? (
 									<>
-										<span>Feels like </span>
-										<Temperature
-											units={units}
-											value={currentWeather.main.feels_like}
-										/>
+										<h6 className="mb-0">Humidity</h6>
+										<h2 className="responsive">
+											{currentWeather.main.humidity}%
+										</h2>
 									</>
-								</h5>
+								) : (
+									<Skeleton count={2} />
+								)}
 							</div>
-						</>
-					) : (
-						<Skeleton containerClassName="flex-1" height={80}></Skeleton>
-					)}
-				</div>
-				<div className="flex flex-row gap-x-3 text-center pt-1">
-					<div className="flex-1">
-						{currentWeather?.weather[0] ? (
-							<>
-								<h6 className="mb-0">Humidity</h6>
-								<h2 className="responsive">{currentWeather.main.humidity}%</h2>
-							</>
+							<div className="flex-grow md:flex-1">
+								{currentWeather?.wind ? (
+									<>
+										<h6 className="mb-0">Winds</h6>
+										<h2 className="responsive align-middle">
+											<span className="inline-block mr-2 mb-1">
+												<img
+													src="/images/noun-wind-direction-4669605.svg"
+													alt=""
+													className="w-4 h-4"
+													style={{
+														rotate: `${currentWeather.wind.deg + 180}deg`,
+													}}
+												/>
+											</span>
+											{currentWeather.wind.speed}
+											{presetUnits.speed}
+										</h2>
+									</>
+								) : (
+									<Skeleton count={2} />
+								)}
+							</div>
+							<div className="flex-1">
+								{currentWeather?.main ? (
+									<>
+										<h6 className="mb-0">Visibility</h6>
+										<h2 className="responsive">
+											{currentWeather.visibility > 1000
+												? `${Math.floor(currentWeather.visibility / 1000)}k${
+														presetUnits.visibility
+												  }`
+												: `${currentWeather.visibility}${presetUnits.visibility}`}
+										</h2>
+									</>
+								) : (
+									<Skeleton count={2} />
+								)}
+							</div>
+						</div>
+						{forecastWeather ? (
+							<div className="mt-4">
+								<h6 className="mb-0">Rest of day</h6>
+								<List className="!px-0 max-h-none md:max-h-none !overflow-hidden">
+									{forecastWeather[Object.keys(forecastWeather)[0]].map(
+										(forecast) => (
+											<Row
+												key={forecast.dt}
+												className="flex flex-row gap-x-2 md:gap-x-4 justify-between text-sm md:text-lg leading-tight ">
+												<span className="w-max">{forecast.lt}</span>
+												<span className="flex-1 flex flex-row items-center justify-center opacity-50">
+													<Temperature
+														units={units}
+														value={forecast.main.temp_min}
+													/>{' '}
+													/{' '}
+													<Temperature
+														units={units}
+														value={forecast.main.temp_max}
+													/>
+												</span>
+												<span className="opacity-75 w-1/3 flex flex-row items-center justify-end text-right">
+													<div className="w-12 h-12 md:w-16 md:h-16">
+														<Image
+															unoptimized
+															width={64}
+															height={64}
+															alt={`Forecast weather at ${forecast.lt}: ${forecast?.weather[0].description}`}
+															src={`https://openweathermap.org/img/wn/${forecast?.weather[0].icon}@2x.png`}
+														/>
+													</div>
+													{forecast.weather[0].description}
+												</span>
+											</Row>
+										),
+									)}
+									<></>
+								</List>
+							</div>
 						) : (
-							<Skeleton count={2} />
-						)}
-					</div>
-					<div className="flex-grow md:flex-1">
-						{currentWeather?.wind ? (
 							<>
-								<h6 className="mb-0">Winds</h6>
-								<h2 className="responsive align-middle">
-									<span className="inline-block mr-2 mb-1">
-										<img
-											src="/images/noun-wind-direction-4669605.svg"
-											alt=""
-											className="w-4 h-4"
-											style={{
-												rotate: `${currentWeather.wind.deg + 180}deg`,
-											}}
-										/>
-									</span>
-									{currentWeather.wind.speed}
-									{presetUnits.speed}
-								</h2>
+								<Skeleton count={4} />
+								<div className="pb-2"></div>
 							</>
-						) : (
-							<Skeleton count={2} />
 						)}
-					</div>
-					<div className="flex-1">
-						{currentWeather?.main ? (
-							<>
-								<h6 className="mb-0">Visibility</h6>
-								<h2 className="responsive">
-									{currentWeather.visibility > 1000
-										? `${Math.floor(currentWeather.visibility / 1000)}k${
-												presetUnits.visibility
-										  }`
-										: `${currentWeather.visibility}${presetUnits.visibility}`}
-								</h2>
-							</>
-						) : (
-							<Skeleton count={2} />
-						)}
-					</div>
-				</div>
-			</Card>
-		</div>
+					</Card>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 }
