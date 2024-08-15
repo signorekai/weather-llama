4dc8984e42b02149003d3316b424416e7cd0bd22
Alfred Lau <alfred@sulphur.com.sg>
2024-08-16 00:57:27 +0800

    :sparkles: tapping on temperature will change between imperial and metric

--- src/components/Temperature.tsx
+++ src/components/Temperature.tsx
@@ -1,21 +1,28 @@
 import useUnits from '@/hooks/units';
+import { useAppStore } from '@/stores/App';
 
 export default function Temperature({
 	value,
-	units = 'metric',
+	units,
 }: Readonly<{
 	value: number;
-	units?: 'metric' | 'imperial';
+	units: 'metric' | 'imperial';
 }>) {
 	let displayValue = value;
 	if (units === 'imperial') {
 		displayValue = value * 1.8 + 32;
 	}
 
+	displayValue = Math.round(displayValue * 10) / 10;
+
+	const [setUnits] = useAppStore((state) => [state.setUnits]);
 	const { temperature } = useUnits();
 
 	return (
-		<span>
+		<span
+			onClick={() => {
+				setUnits(units === 'metric' ? 'imperial' : 'metric');
+			}}>
 			{displayValue}
 			<span className="temp-label">{temperature}</span>
 		</span>
