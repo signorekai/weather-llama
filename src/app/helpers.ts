a420a6b93028d17aededbfc5034e08dca44e5313
Alfred Lau <alfred@sulphur.com.sg>
2024-08-16 18:39:34 +0800

    :recycle: move to helper file to cater to edge cases

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,4 +1,9 @@
 import type { City, CityWithName } from '@/types/City';
+import { DateTime } from 'luxon';
+
+import countries from 'i18n-iso-countries';
+import english from 'i18n-iso-countries/langs/en.json';
+countries.registerLocale(english);
 
 export const addNameToCity = (city: City): CityWithName => {
 	return {
@@ -7,4 +12,52 @@ export const addNameToCity = (city: City): CityWithName => {
 			typeof city.state !== 'undefined' ? `, ${city.state}` : ''
 		}, ${city.country}`,
 	};
+};
+
+interface GetValidLocalDateTimeParamBasic {
+	cityName: string;
+}
+
+interface GetValidLocalDateTimeParamWithCountryName
+	extends GetValidLocalDateTimeParamBasic {
+	countryName: string;
+	countryCode?: never;
+}
+
+interface GetValidLocalDateTimeParamWithCountryCode
+	extends GetValidLocalDateTimeParamBasic {
+	countryCode: string;
+	countryName?: never;
+}
+
+type GetValidLocalDateTimeParam =
+	| GetValidLocalDateTimeParamWithCountryCode
+	| GetValidLocalDateTimeParamWithCountryName;
+
+export const getValidLocalDateTime = (
+	params: GetValidLocalDateTimeParam,
+): [DateTime, countryName: string, IANA: string] => {
+	const countryName = params.countryName
+		? params.countryName
+		: countries.getName(params.countryCode!, 'en') || '';
+
+	const { cityName, countryCode = '' } = params;
+
+	const IANA = `${countryName}/${cityName}`;
+
+	let today = DateTime.now().setZone(IANA);
+	if (!today.isValid) {
+		today = DateTime.now().setZone(countryName);
+	}
+
+	if (!today.isValid) {
+		today = DateTime.now().setZone(countryCode);
+	}
+
+	if (!today.isValid) {
+		console.log(43, today, IANA, countryName);
+		throw new Error('Invalid country code / country name and city name!');
+	}
+
+	return [today, countryName, IANA];
 };
