6201e7749db6cdc1d9566f51b414ff78989123f5
Alfred Lau <alfred@sulphur.com.sg>
2024-08-17 00:11:22 +0800

    :sparkles: added error boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import { useState, useCallback, useEffect } from 'react';
 import countries from 'i18n-iso-countries';
 import english from 'i18n-iso-countries/langs/en.json';
 
+import ErrorModal from '@/components/ErrorModal';
+import { ErrorBoundary, useErrorBoundary } from 'react-error-boundary';
+
 import WeatherToday from '@/components/WeatherToday';
 import { useAppStore } from '@/stores/App';
 import { Forecast, SortedForecast } from '@/types/Forecast';
@@ -55,8 +58,6 @@ export default function Home() {
 				cityName: data.city.name,
 				secondsOffset,
 			});
-
-			console.log(51, today);
 
 			const endOfToday = today.endOf('day');
 			const days = [
@@ -123,7 +124,10 @@ export default function Home() {
 						data: forecast.data,
 						secondsOffset: current.data.timezone,
 					});
-				} catch (error) {}
+				} catch (error: any) {
+					// console.log(132, error);
+					// setError();
+				}
 			};
 			fetchData();
 		}
@@ -147,84 +151,86 @@ export default function Home() {
 	};
 
 	return (
-		<main className="flex flex-1 flex-col items-center justify-between p-4 md:p-8">
-			<SkeletonTheme
-				highlightColor={
-					darkMode
-						? skeletonColours.dark.highlight
-						: skeletonColours.light.highlight
-				}
-				baseColor={
-					darkMode ? skeletonColours.dark.base : skeletonColours.light.base
-				}>
-				<WeatherToday
-					currentWeather={currentWeather}
-					forecastWeather={forecastWeather}
-				/>
-			</SkeletonTheme>
-			{forecastWeather && (
-				<div className="grid grid-cols-1 gap-x-2 gap-y-2 mt-2 items-center w-full max-w-screen-lg mx-auto">
-					{Object.keys(forecastWeather)
-						.slice(1)
-						.filter((k) => forecastWeather[k].length > 0)
-						.map((k) => (
-							<WeatherForecast
-								className="mx-auto"
-								key={k}
-								forecastWeather={forecastWeather[k]}
-							/>
-						))}
+		<ErrorBoundary FallbackComponent={ErrorModal}>
+			<main className="flex flex-1 flex-col items-center justify-between p-4 md:p-8">
+				<SkeletonTheme
+					highlightColor={
+						darkMode
+							? skeletonColours.dark.highlight
+							: skeletonColours.light.highlight
+					}
+					baseColor={
+						darkMode ? skeletonColours.dark.base : skeletonColours.light.base
+					}>
+					<WeatherToday
+						currentWeather={currentWeather}
+						forecastWeather={forecastWeather}
+					/>
+				</SkeletonTheme>
+				{forecastWeather && (
+					<div className="grid grid-cols-1 gap-x-2 gap-y-2 mt-2 items-center w-full max-w-screen-lg mx-auto">
+						{Object.keys(forecastWeather)
+							.slice(1)
+							.filter((k) => forecastWeather[k].length > 0)
+							.map((k) => (
+								<WeatherForecast
+									className="mx-auto"
+									key={k}
+									forecastWeather={forecastWeather[k]}
+								/>
+							))}
+					</div>
+				)}
+				<div className="mt-6 text-xs credits">
+					<h6 className="mb-0">Image credits</h6>
+					<ul className="opacity-35">
+						<li>
+							Llama by havid ika from{' '}
+							<a
+								href="https://thenounproject.com/icon/llama-4171316/"
+								target="_blank"
+								rel="noopener noreferrer"
+								title="Llama Icons">
+								Noun Project
+							</a>{' '}
+							(CC BY 3.0)
+						</li>
+						<li>
+							spinner by Zach Bogart from{' '}
+							<a
+								href="https://thenounproject.com/icon/spinner-3644820/"
+								target="_blank"
+								rel="noopener noreferrer"
+								title="spinner Icons">
+								Noun Project
+							</a>{' '}
+							(CC BY 3.0)
+						</li>
+						<li>
+							Trash by Sebastiaan van Veen from{' '}
+							<a
+								href="https://thenounproject.com/browse/icons/term/trash/"
+								target="_blank"
+								rel="noopener noreferrer"
+								title="Trash Icons">
+								Noun Project
+							</a>{' '}
+							(CC BY 3.0)
+						</li>
+						<li>
+							wind direction by Komardews from{' '}
+							<a
+								href="https://thenounproject.com/browse/icons/term/wind-direction/"
+								target="_blank"
+								rel="noopener noreferrer"
+								title="wind direction Icons">
+								Noun Project
+							</a>{' '}
+							(CC BY 3.0)
+						</li>
+					</ul>
 				</div>
-			)}
-			<div className="mt-6 text-xs credits">
-				<h6 className="mb-0">Image credits</h6>
-				<ul className="opacity-35">
-					<li>
-						Llama by havid ika from{' '}
-						<a
-							href="https://thenounproject.com/icon/llama-4171316/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="Llama Icons">
-							Noun Project
-						</a>{' '}
-						(CC BY 3.0)
-					</li>
-					<li>
-						spinner by Zach Bogart from{' '}
-						<a
-							href="https://thenounproject.com/icon/spinner-3644820/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="spinner Icons">
-							Noun Project
-						</a>{' '}
-						(CC BY 3.0)
-					</li>
-					<li>
-						Trash by Sebastiaan van Veen from{' '}
-						<a
-							href="https://thenounproject.com/browse/icons/term/trash/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="Trash Icons">
-							Noun Project
-						</a>{' '}
-						(CC BY 3.0)
-					</li>
-					<li>
-						wind direction by Komardews from{' '}
-						<a
-							href="https://thenounproject.com/browse/icons/term/wind-direction/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="wind direction Icons">
-							Noun Project
-						</a>{' '}
-						(CC BY 3.0)
-					</li>
-				</ul>
-			</div>
-		</main>
+			</main>
+		</ErrorBoundary>
 	);
 }
---
+++ src/components/ErrorModal.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { Card } from './Card';
+
+export default function ErrorModal({
+	error,
+	resetErrorBoundary,
+}: {
+	error: Error;
+	resetErrorBoundary: () => void;
+}) {
+	return (
+		<div className="mt-4 lg:max-w-screen-sm w-full mx-auto">
+			<AnimatePresence>
+				{error && (
+					<motion.div
+						key="error-container"
+						initial={{ opacity: 0, translateY: 40, scale: 0.9 }}
+						animate={{ opacity: 1, translateY: 0, scale: 1 }}
+						exit={{ opacity: 0, translateY: 40, scale: 0.9 }}>
+						<Card className="px-4 py-4 text-center">{error.message}</Card>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</div>
+	);
+}
